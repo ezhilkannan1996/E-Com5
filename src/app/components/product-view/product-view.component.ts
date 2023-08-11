5a0f8f3a2bd794826283ff293c785279aef6a415
ezhil.k@swordgroup.in <ezhil.k@swordgroup.in>
2023-08-11 15:21:04 +0530

    First commit

---
+++ src/app/components/product-view/product-view.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-view',
+  templateUrl: './product-view.component.html',
+  styleUrls: ['./product-view.component.css']
+})
+export class ProductViewComponent implements OnInit {
+  product: any; // Assuming a similar data structure for the product
+
+  constructor(private route: ActivatedRoute, private productService: ProductService,private cartService :CartService) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const productId = Number(params.get('id'));
+      this.product = this.productService.getProductById(productId);
+    });
+  }
+  addToCart(product: any): void {
+    this.cartService.addToCart(product);
+  }
+
+  isProductAddedToCart(productId: string): boolean {
+    return this.cartService.isProductAdded(productId);
+  }
+
+  isProductInCart(productId: string): boolean {
+    return this.cartService.isProductAdded(productId);
+  }
+}
